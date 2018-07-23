dd7b57b4c9cae0bf634d2b7deecaaf5bb79c1c74
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2018-07-23 14:04:48 +0200

    2d plot also working now.

--- plottr/plot/mpl.py
+++ plottr/plot/mpl.py
@@ -1,19 +1,27 @@
 from pprint import pprint
 
+import numpy as np
+
 from matplotlib import rcParams
+from matplotlib import cm
+from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavBar
 from matplotlib.figure import Figure
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from pyqtgraph.Qt import QtGui, QtCore
 
 from ..node.node import Node
 
+# TODO:
+# * plot properties should be configurable
+
 ### matplotlib tools
 def setMplDefaults():
-    rcParams['figure.dpi'] = 150
+    rcParams['figure.dpi'] = 300
     rcParams['figure.figsize'] = (4.5, 3)
-    rcParams['savefig.dpi'] = 150
+    rcParams['savefig.dpi'] = 300
     rcParams['axes.grid'] = True
     rcParams['grid.linewidth'] = 0.5
     rcParams['grid.linestyle'] = ':'
@@ -40,6 +48,21 @@ def pcolorgrid(xaxis, yaxis):
     yedges = centers2edges(yaxis)
     xx, yy = np.meshgrid(xedges, yedges)
     return xx, yy
+
+def ppcolormesh(ax, x, y, z, cmap=None, make_grid=True, **kw):
+    if cmap is None:
+        cmap = cm.viridis
+
+    if make_grid:
+        _x, _y = pcolorgrid(x, y)
+    else:
+        _x, _y = x, y
+
+    im = ax.pcolormesh(_x, _y, z, cmap=cmap, **kw)
+    ax.set_xlim(_x.min(), _x.max())
+    ax.set_ylim(_y.min(), _y.max())
+
+    return im
 
 
 class PlotNode(Node):
@@ -72,16 +95,31 @@ class MPLPlot(FCanvas):
         self.clearFig(nrows, ncols)
         self.setParent(parent)
 
-    def clearFig(self, nrows=1, ncols=1):
+    def clearFig(self, nrows=1, ncols=1, naxes=1):
         self.fig.clear()
 
         self.axes = []
-        naxes = nrows * ncols
-        for i in range(1,nrows+1):
-            for j in range(1,ncols+1):
-                self.axes.append(self.fig.add_subplot(naxes, i, j))
+        iax = 1
+        if naxes > nrows * ncols:
+            raise ValueError(f'Number of axes ({naxes}) larger than rows ({nrows}) x columns ({ncols}).')
 
+        for i in range(1,naxes+1):
+            kw = {}
+            if iax > 1:
+                kw['sharex'] = self.axes[0]
+                kw['sharey'] = self.axes[0]
+
+            self.axes.append(self.fig.add_subplot(nrows, ncols, i))
+            iax += 1
+
+        self.fig.tight_layout()
         self.draw()
+        return self.axes
+
+    def resizeEvent(self, event):
+        self.fig.tight_layout()
+        super().resizeEvent(event)
+
 
 
 class MPLPlotWidget(QtGui.QWidget):
@@ -121,7 +159,23 @@ class AutoPlot(MPLPlotWidget):
         ax.set_ylabel(ylabel)
         ax.set_xlabel(axName + " ({})".format(data[axName]['unit']))
         ax.legend()
+        self.plot.fig.tight_layout()
 
+    def _plot2d(self, data, ax, xName, yName, dName):
+        x = data[xName]['values']
+        y = data[yName]['values']
+        zz = data[dName]['values'].T
+
+        im = ppcolormesh(ax, x, y, zz)
+        div = make_axes_locatable(ax)
+        cax = div.append_axes("right", size="5%", pad=0.05)
+        self.plot.fig.colorbar(im, cax=cax)
+
+        ax.set_title(dName, size='small')
+        ax.set_ylabel(yName + " ({})".format(data[yName]['unit']))
+        ax.set_xlabel(xName + " ({})".format(data[xName]['unit']))
+        cax.set_ylabel(dName + " ({})".format(data[dName]['unit']))
+    
 
     def setData(self, data):
         axesNames = data.axes_list()
@@ -130,9 +184,22 @@ class AutoPlot(MPLPlotWidget):
         naxes = len(axesNames)
         ndata = len(dataNames)
 
-        if naxes == 1:
-            self.plot.clearFig(1, 1)
-            ax = self.plot.axes[0]
-            self._plot1d(data, ax, axesNames[0], dataNames)
+        if naxes == 0 or ndata == 0:
+            self.plot.clearFig(naxes=0)
 
+        elif naxes == 1:
+            ax = self.plot.clearFig(1, 1, 1)[0]
+            self._plot1d(data, ax, axesNames[0], dataNames)
+        elif naxes == 2:
+            nrows = ndata**.5//1
+            ncols = np.ceil(ndata/nrows)
+            axes = self.plot.clearFig(nrows, ncols, ndata)
+            for i, dn in enumerate(dataNames):
+                ax = axes[i]
+                self._plot2d(data, ax, axesNames[0], axesNames[1], dn)
+                
+        elif naxes > 2:
+            raise ValueError('Cannot plot more than two axes. (given: {})'.format(axesNames))
+
+        self.plot.fig.tight_layout()
         self.plot.draw()
