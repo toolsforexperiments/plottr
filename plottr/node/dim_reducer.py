39cdf318adea9b6916fb5d198940c04f9918139d
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2018-10-14 10:51:17 +0200

    - can also remove axes from non-grid data - implementation of x/y selector - minor fixes - more consequent use of logging.

--- plottr/node/dim_reducer.py
+++ plottr/node/dim_reducer.py
@@ -18,14 +18,21 @@ __author__ = 'Wolfgang Pfaff'
 __license__ = 'MIT'
 
 
-def selectAxisElement(arr, axis=0, index=0):
+def sliceAxis(arr, sliceObj, axis):
+    """
+    return the array where the axis with the given index is sliced
+    with the given slice object.
+    """
+    slices = [np.s_[::] for i in arr.shape]
+    slices[axis] = sliceObj
+    return arr[tuple(slices)]
+
+def selectAxisElement(arr, index, axis):
     """
     return the squeezed array where the given axis has been reduced to its
     value with the given index.
     """
-    slices = [np.s_[::] for i in arr.shape]
-    slices[axis] = np.s_[index:index+1:]
-    return np.squeeze(arr[slices])
+    return np.squeeze(sliceAxis(arr, np.s_[index:index+1:], axis))
 
 
 class DimensionReducer(Node):
@@ -35,7 +42,8 @@ class DimensionReducer(Node):
     Each axis can be assigned an arbitrary reduction function that will reduce the
     axis to a single value. For each assigned reduction the dimension shrinks by 1.
 
-    If the input is not GridData, data is simply passed through.
+    If the input is not GridData, data is just passed through, but we delete the
+    axes present in reductions.
 
     Reductions affect all data fields that are given to the property targetNames (as list).
     if targetNames is None (default), then we'll try to apply the reduction to all
@@ -55,6 +63,7 @@ class DimensionReducer(Node):
 
         self._reductions = {}
         self._targetNames = None
+        self._protectedAxes = []
 
     @property
     def reductions(self):
@@ -80,8 +89,50 @@ class DimensionReducer(Node):
         else:
             dnames = data.dependents()
 
+        if not isinstance(data, GridDataDict):
+            self.logger().debug(f"Data is not on a grid. Reduction functions are ignored, axes will simply be removed.")
+
         for n in dnames:
             for ax, reduction in self._reductions.items():
+                fun, arg, kw = reduction
+                try:
+                    idx = data[n]['axes'].index(ax)
+                except IndexError:
+                    self.logger().info(f'{ax} specified for reduction, but not present in data; ignore.')
+
+                kw['axis'] = idx
+
+                if isinstance(data, GridDataDict):
+                    # check that the new shape is actually correct
+                    # get target shape by removing the right axis
+                    targetShape = list(data[n]['values'].shape)
+                    del targetShape[idx]
+                    targetShape = tuple(targetShape)
+
+                    newvals = fun(data[n]['values'], *arg, **kw)
+                    if newvals.shape != targetShape:
+                        self.logger().error(
+                            f'Reduction on axis {ax} did not result in the right data shape. ' +
+                            f'Expected {targetShape} but got {newvals.shape}.'
+                            )
+                        return None
+                    data[n]['values'] = newvals
+
+                del data[n]['axes'][idx]
+
+        data.remove_unused_axes()
+        data.validate()
+        return data
+
+
+    def validateOptions(self, data):
+        """
+        Checks performed:
+        * each item in reduction must be of the form (fun, [*arg], {**kw}),
+          with arg and kw being optional.
+        """
+        for ax, reduction in self._reductions.items():
+            try:
                 fun = reduction[0]
                 if len(reduction) == 1:
                     arg = []; kw = {}
@@ -89,25 +140,24 @@ class DimensionReducer(Node):
                     arg = reduction[1]; kw = {}
                 else:
                     arg = reduction[1]; kw = reduction[2]
+            except:
+                self.logger().warning(f'Reduction for axis {ax} not in the right format.')
+                return False
 
-                idx = data[n]['axes'].index(ax)
-                kw['axis'] = idx
-                data[n]['values'] = fun(data[n]['values'], *arg, **kw)
-                del data[n]['axes'][idx]
+            self._reductions[ax] = (fun, arg, kw)
 
-        data.remove_unused_axes()
-        data.validate()
-        return data
+        return True
 
     def process(self, **kw):
-        data = kw['dataIn']
+        data = super().process(**kw)
+        if data is None:
+            return None
 
-        if not isinstance(data, GridDataDict):
-            return dict(dataOut=data)
+        data = self._applyDimReductions(copy.deepcopy(data['dataOut']))
 
-        data = self._applyDimReductions(copy.deepcopy(data))
+        if data is None:
+            return None
         return dict(dataOut=data)
-
 
 
 class XYAxesSelector(DimensionReducer):
@@ -126,15 +176,72 @@ class XYAxesSelector(DimensionReducer):
     nodeName = 'XYAxesSelector'
 
     def __init__(self, *arg, **kw):
-        super().__init__(self, *arg, **kw)
+        super().__init__(*arg, **kw)
 
-        self._xAxis = None
-        self._yAxis = None
+        self._xyAxes = None, None
+
+    @property
+    def xyAxes(self):
+        return self._xyAxes
+
+    @xyAxes.setter
+    @Node.updateOption('xyAxes')
+    def xyAxes(self, val):
+        self._xyAxes = val
+
+    def validateOptions(self, data):
+        """
+        Checks performed:
+        * values for xAxis and yAxis must be axes that exist for the input data.
+        * x/y axes cannot be the same
+        * x/y axes cannot be reduced (will be removed from reductions)
+        * all axes that are not x/y must be reduced (defaulting to selection of the first element)
+        """
+        if not super().validateOptions(data):
+            return False
+
+        availableAxes = data.axes_list()
+        if self._xyAxes[0] is None:
+            self.logger().debug(f'x-Axis is None. this will result in empty output data.')
+            return False
+        elif self._xyAxes[0] not in availableAxes:
+            self.logger().warning(f'x-Axis {self._xyAxis[0]} not present in data')
+            return False
+
+        if self._xyAxes[1] is None:
+            self.logger().debug(f'y-Axis is None; result will be 1D')
+        elif self._xyAxes[1] not in availableAxes:
+            self.logger().warning(f'y-Axis {self._xyAxes[1]} not present in data')
+            return False
+        elif self._xyAxes[1] == self._xyAxes[0]:
+            self.logger().warning(f"y-Axis cannot be equal to x-Axis.")
+            return False
+
+        if isinstance(data, GridDataDict):
+            delete = []
+            for n, _ in self.reductions.items():
+                if n in self._xyAxes:
+                    self.logger().debug(f"{n} has been selected as axis, cannot be reduced.")
+                    delete.append(n)
+            for n in delete:
+                del self._reductions[n]
+
+            for ax in availableAxes:
+                if ax not in self._xyAxes and ax not in self.reductions:
+                    self.logger().debug(f"{ax} must be reduced. Default to selecting first element.")
+                    self._reductions[ax] = (selectAxisElement, [], dict(index=0))
+
+        return True
 
     def process(self, **kw):
-        data = copy.deepcopy(kw['dataIn'])
+        data = super().process(**kw)
+        if data is None:
+            return None
 
-        # no x-axis is invalid. because i say so.
-        if self._xSelection in ['', None]:
-            return dict(dataOut=DataDict())
+        data = data['dataOut']
+        if self._xyAxes[0] is not None and self._xyAxes[1] is not None:
+            _kw = {self._xyAxes[0]: 0, self._xyAxes[1]: 1}
+            data.reorder_axes(**_kw)
+
+        return dict(dataOut=data)
 
