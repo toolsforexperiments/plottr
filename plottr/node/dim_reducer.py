ba31cfc4b1b6f97d05118c0181b2835885038dd3
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2018-10-24 08:22:00 +0200

    somewhat working and complete xy selector now. still buggy, but basics in place.

--- plottr/node/dim_reducer.py
+++ plottr/node/dim_reducer.py
@@ -5,6 +5,7 @@ A node and widget for reducing data to 1d/2d data.
 """
 import copy
 from pprint import pprint
+import enum
 
 import numpy as np
 
@@ -18,6 +19,19 @@ __author__ = 'Wolfgang Pfaff'
 __license__ = 'MIT'
 
 
+# FIXME:
+# * when de-selecting all data in in the dataselector, options disappear in the XYsel
+#   gui. that is OK. but when we now select different data, the selection is still
+#   in the node, but is not reflected in the node until we make a change.
+#   it would be ok for the options to either persist when no data is selected,
+#   OR to make the correct selections appear after re-selecting.
+# * ungridded data does not play well with the gui yet. somehow when going from
+#   gridded data to ungridded, the reductions/axes are not set correctly.
+#   in particular, the data selectAxis reduction pops up.
+#   de-selecting an axis also does not default to 'ignore' then.
+
+
+# Some helpful reduction functions
 def sliceAxis(arr, sliceObj, axis):
     """
     return the array where the axis with the given index is sliced
@@ -33,6 +47,23 @@ def selectAxisElement(arr, index, axis):
     value with the given index.
     """
     return np.squeeze(sliceAxis(arr, np.s_[index:index+1:], axis))
+
+
+# Translation between reduction functions and some name we like to use
+# to address them
+axisReductionFuncs = {
+    'select value' : selectAxisElement,
+    'average' : np.mean,
+}
+
+def reductionFuncFromName(name):
+    return axisReductionFuncs.get(name, None)
+
+def reductionNameFromFunc(func):
+    for n, f in axisReductionFuncs.items():
+        if f == func:
+            return n
+    return None
 
 
 class DimensionReducer(Node):
@@ -59,18 +90,21 @@ class DimensionReducer(Node):
     nodeName = 'DimensionReducer'
 
     def __init__(self, *arg, **kw):
-        super().__init__(*arg, **kw)
 
         self._reductions = {}
         self._targetNames = None
         self._protectedAxes = []
+
+        super().__init__(*arg, **kw)
+
+    # Properties
 
     @property
     def reductions(self):
         return self._reductions
 
     @reductions.setter
-    @Node.updateOption()
+    @Node.updateOption('reductions')
     def reductions(self, val):
         self._reductions = val
 
@@ -82,6 +116,23 @@ class DimensionReducer(Node):
     @Node.updateOption()
     def targetNames(self, val):
         self._targetNames = val
+
+    # Tools for passing reductions -- mapping to strings and back
+    @staticmethod
+    def encodeReductions(reductions):
+        red = {}
+        for k, (f, arg, kw) in reductions.items():
+            red[k] = (reductionNameFromFunc(f), arg, kw)
+        return red
+
+    @staticmethod
+    def decodeReductions(reductions):
+        red = {}
+        for k, (n, arg, kw) in reductions.items():
+            red[k] = (reductionFuncFromName(n), arg, kw)
+        return red
+
+    # Data processing
 
     def _applyDimReductions(self, data):
         if self._targetNames is not None:
@@ -124,7 +175,6 @@ class DimensionReducer(Node):
         data.validate()
         return data
 
-
     def validateOptions(self, data):
         """
         Checks performed:
@@ -160,6 +210,283 @@ class DimensionReducer(Node):
         return dict(dataOut=data)
 
 
+class XYAxesSelectionWidget(QtGui.QTreeWidget):
+
+    options = ['', 'x-axis', 'y-axis', 'average', 'select value']
+    gridOnlyOptions = ['average', 'select value']
+
+    # signals for when the user has changed options via the UI.
+    xyAxesChanged = QtCore.pyqtSignal(tuple)
+    reductionsChanged = QtCore.pyqtSignal(dict)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setColumnCount(4)
+        self.setHeaderLabels(['Axis', 'Role', 'Options', 'Info'])
+
+        self._dataStructure = None
+        self._grid = True
+        self._emitChoiceChange = True
+
+        self.choices = {}
+
+    def clear(self):
+        super().clear()
+        for n, opts in self.choices.items():
+            opts['role'].deleteLater()
+            del opts['role']
+
+        self.choices = {}
+
+    def updateSizes(self):
+        for i in range(4):
+            self.resizeColumnToContents(i)
+
+    def addAxis(self, name, grid=True):
+        item = QtGui.QTreeWidgetItem([name, '', '', ''])
+        self.addTopLevelItem(item)
+
+        combo = QtGui.QComboBox()
+        for o in self.options:
+            if not grid and o in self.gridOnlyOptions:
+                pass
+            else:
+                combo.addItem(o)
+        combo.setMinimumSize(50, 30)
+        self.setItemWidget(item, 1, combo)
+        self.updateSizes()
+
+        self.choices[name] = {'role' : combo, 'options' : None}
+        combo.currentTextChanged.connect(lambda x: self.roleChanged(name, x))
+
+    # Generic role handling
+
+    def _setRole(self, ax, role=None, **kw):
+        if role is None:
+            role = ''
+
+        item = self.findItems(ax, QtCore.Qt.MatchExactly, 0)[0]
+
+        roleHasChanged = False
+        if self.choices[ax]['role'].currentText() != role:
+            self.choices[ax]['role'].setCurrentText(role)
+
+        self.setInfo(ax, '')
+
+        w = self.choices[ax]['options']
+        if w is not None: #  and roleHasChanged:
+            # not entirely sure yet about this procedure.
+            # i have seen crashes seen before with similar constructions.
+            # need to keep an eye on this.
+            self.setItemWidget(item, 2, None)
+            w.deleteLater()
+            self.choices[ax]['options'] = None
+            self.updateSizes()
+
+        if role == 'select value': # and w is None:
+            # TODO: set slider from current value?
+            w = self._axisSlider(self._dataStructure[ax]['info']['shape'][0])
+            self.choices[ax]['options'] = w
+            self.setItemWidget(item, 2, w)
+            w.setMinimumSize(150, 30)
+            self.updateSizes()
+
+            w.valueChanged.connect(lambda x: self.axisValueSelected(ax, x))
+            w.valueChanged.emit(w.value())
+
+    def _getRole(self, ax):
+        return self.choices[ax]['role'].currentText()
+
+    @QtCore.pyqtSlot(str, str)
+    def setInfo(self, ax, info):
+        item = self.findItems(ax, QtCore.Qt.MatchExactly, 0)[0]
+        item.setText(3, info)
+
+    @QtCore.pyqtSlot(dict)
+    def setInfos(self, infos):
+        for ax, info in infos.items():
+            self.setInfo(ax, info)
+
+    @QtCore.pyqtSlot(str, str)
+    def roleChanged(self, axis, newSelection):
+        """
+        when a user changes the selection on a role combo box manually,
+        we need to check whether we need to update other roles, and update
+        role options.
+        """
+        if self._emitChoiceChange:
+
+            # cannot have multiple axes selected as x or y
+            self._setRole(axis, newSelection)
+            if newSelection in ['x-axis', 'y-axis']:
+                for ax, opt in self.choices.items():
+                    if ax != axis and opt['role'].currentText() == newSelection:
+
+                        # this is a programmatic change of role,
+                        # so we should make sure we don't tell the node yet.
+                        self._emitChoiceChange = False
+                        self._setRole(ax, None)
+                        self._emitChoiceChange = True
+
+                # this can only happen when newSelection is x/y axis.
+                self.xyAxesChanged.emit(self._getXY())
+
+            # any role change basically results in a change in reductions.
+            self.reductionsChanged.emit(self._getReductions())
+
+    # Data structure handling
+
+    def _enableGrid(self, ax, enable=True):
+        """
+        When data is gridded, we can use axes reductions.
+        When it's not, we want to hide options that aren't applicable.
+        """
+        combo = self.choices[ax]['role']
+        for o in self.gridOnlyOptions:
+            idx = combo.findText(o)
+            if enable and idx < 0:
+                combo.addItem(o)
+            elif not enable and idx >= 0:
+                if combo.currentText() in self.gridOnlyOptions:
+                    self._setRole(ax, None)
+                combo.removeItem(idx)
+
+    def setDataStructure(self, structure, grid):
+        """
+        This function populates the axes items and the
+        role selection widgets correctly.
+        """
+        # TODO: things when data size changes
+
+        _set = False
+        if structure is None or self._dataStructure is None:
+            _set = True
+        elif structure.axes_list() != self._dataStructure.axes_list():
+            _set = True
+        if _set:
+            self.clear()
+            if structure is not None:
+                for ax in structure.axes_list():
+                    self.addAxis(ax, grid=grid)
+
+        self._dataStructure = structure
+
+        if grid != self._grid:
+            self._grid = grid
+            for ax in self._dataStructure.axes_list():
+                self._enableGrid(ax, self._grid)
+
+    # Handling of particular options
+
+    # XY axes
+
+    @QtCore.pyqtSlot(str)
+    def setXYAxes(self, xy):
+        """
+        Programatically set x and y axes in the roles.
+        Do not emit a signal about UI element changes.
+        """
+        self._emitChoiceChange = False
+        x, y = xy
+        for ax, opts in self.choices.items():
+            if ax == x and opts['role'].currentText() != 'x-axis':
+                self._setRole(ax, 'x-axis')
+            if ax != x and opts['role'].currentText() == 'x-axis':
+                self._setRole(ax, None)
+            if ax == y and opts['role'].currentText() != 'y-axis':
+                self._setRole(ax, 'y-axis')
+            if ax != y and opts['role'].currentText() == 'y-axis':
+                self._setRole(ax, None)
+
+        self._emitChoiceChange = True
+
+    def _getXY(self):
+        x = None; y = None
+        for ax, opt in self.choices.items():
+            if opt['role'].currentText() == 'x-axis':
+                x = ax
+            elif opt['role'].currentText() == 'y-axis':
+                y = ax
+
+        return x, y
+
+    # Reductions
+
+    @QtCore.pyqtSlot(dict)
+    def setReductions(self, reductions):
+        """
+        Programatically set reduction roles.
+        Do not emit signals about UI element changes.
+
+        reductions are expected in the form the DimensionReducer class
+        stores them internally (unencoded!).
+        """
+        self._emitChoiceChange = False
+
+        # get a string-representation of the reduction functions
+        red = DimensionReducer.encodeReductions(reductions)
+
+        # now can set the role by name for each reduction
+        for ax, (name, args, kwargs) in red.items():
+            if self._getRole(ax) != name:
+                self._setRole(ax, name)
+
+            # now we can process options for reductions that have any.
+            # value selection uses a slider
+            if name == 'select value':
+                slider = self.choices[ax]['options']
+                if slider.value() != kwargs['index']:
+                    slider.setValue(kwargs['index'])
+
+        self._emitChoiceChange = True
+
+    def _getReductions(self):
+        ret = {}
+        for ax, opt in self.choices.items():
+            role = self._getRole(ax)
+            if role not in ['x-axis', 'y-axis', '', None]:
+                ret[ax] = [role, [], {}]
+
+                # Special options below, if applicable
+                # value selection: has a slider, value is index
+                if role == 'select value':
+                    slider = self.choices[ax]['options']
+                    if slider is not None:
+                        idx = slider.value()
+                    else:
+                        idx = 0
+                    ret[ax][2] = dict(index=idx)
+
+                ret[ax] = tuple(ret[ax])
+
+        return ret
+
+
+    # value selection
+
+    def _axisSlider(self, npts, value=0):
+        """
+        Return a new axis slider widget.
+        """
+        w = QtGui.QSlider(0x01)
+        w.setMinimum(0)
+        w.setMaximum(npts-1)
+        w.setSingleStep(1)
+        w.setPageStep(1)
+        w.setTickInterval(10)
+        w.setTickPosition(QtGui.QSlider.TicksBelow)
+        w.setValue(value)
+        return w
+
+    @QtCore.pyqtSlot(str, int)
+    def axisValueSelected(self, ax, idx):
+        info = f"{idx+1}/{self._dataStructure[ax]['info']['shape'][0]}"
+        self.setInfo(ax, info)
+        if self._emitChoiceChange:
+            self.reductionsChanged.emit(self._getReductions())
+
+
 class XYAxesSelector(DimensionReducer):
     """
     A Node that allows the user to select one or two axes (x and/or y); these
@@ -174,11 +501,30 @@ class XYAxesSelector(DimensionReducer):
     """
 
     nodeName = 'XYAxesSelector'
+    dataStructureChanged = QtCore.pyqtSignal(dict, bool)
+    sendAxisInfo = QtCore.pyqtSignal(dict)
+
+    uiClass = XYAxesSelectionWidget
+    guiOptions = {
+        'xyAxes' : {
+            'widget' : None,
+            'setFunc' : 'setXYAxes',
+        },
+        'reductions' : {
+            'widget' : None,
+            'setFunc' : 'setReductions',
+        }
+    }
 
     def __init__(self, *arg, **kw):
-        super().__init__(*arg, **kw)
 
         self._xyAxes = None, None
+        self._dataStructure = None
+        self._grid = True
+
+        super().__init__(*arg, **kw)
+
+    # properties
 
     @property
     def xyAxes(self):
@@ -189,6 +535,8 @@ class XYAxesSelector(DimensionReducer):
     def xyAxes(self, val):
         self._xyAxes = val
 
+    # data processing
+
     def validateOptions(self, data):
         """
         Checks performed:
@@ -197,6 +545,7 @@ class XYAxesSelector(DimensionReducer):
         * x/y axes cannot be reduced (will be removed from reductions)
         * all axes that are not x/y must be reduced (defaulting to selection of the first element)
         """
+
         if not super().validateOptions(data):
             return False
 
@@ -217,6 +566,12 @@ class XYAxesSelector(DimensionReducer):
             self.logger().warning(f"y-Axis cannot be equal to x-Axis.")
             return False
 
+        # below we actually mess with the reduction options, but
+        # without using the decorated property.
+        # make sure we emit the right signal at the end.
+        reductionsChanged = False
+
+        # Check: an axis marked as x/y cannot be also reduced.
         delete = []
         for n, _ in self.reductions.items():
             if n in self._xyAxes:
@@ -224,16 +579,35 @@ class XYAxesSelector(DimensionReducer):
                 delete.append(n)
         for n in delete:
             del self._reductions[n]
+            reductionsChanged = True
 
+        # check: axes not marked as x/y should all be reduced.
         for ax in availableAxes:
             if ax not in self._xyAxes and ax not in self.reductions:
                 self.logger().debug(f"{ax} must be reduced. Default to selecting first element.")
                 self._reductions[ax] = (selectAxisElement, [], dict(index=0))
+                reductionsChanged = True
+
+        if reductionsChanged:
+            self.optionChanged.emit('reductions', self._reductions)
+
+        # some output infos for the reduction widgets
+        infos = {}
+        reductionsEncoded = DimensionReducer.encodeReductions(self._reductions)
+        for ax, item in reductionsEncoded.items():
+            if item[0] == 'select value':
+                idx = item[2].get('index')
+                infos[ax] = (f"pt. {idx+1}/{len(data[ax]['values'])}"
+                             f" ({data[ax]['values'][idx]} {data[ax]['unit']})")
+        self.sendAxisInfo.emit(infos)
 
         return True
 
     def process(self, **kw):
-        data = super().process(**kw)
+        data = kw['dataIn']
+        self.updateUi(data)
+
+        data = super().process(dataIn=data)
         if data is None:
             return None
 
@@ -243,4 +617,27 @@ class XYAxesSelector(DimensionReducer):
             data.reorder_axes(**_kw)
 
         return dict(dataOut=data)
+
+    # GUI interaction
+
+    def setupUi(self):
+        self.dataStructureChanged.connect(self.ui.setDataStructure)
+        self.ui.setXYAxes(self._xyAxes)
+        self.ui.xyAxesChanged.connect(self._setXYAxes)
+        self.ui.setReductions(self._reductions)
+        self.ui.reductionsChanged.connect(self._setReductions)
+        self.sendAxisInfo.connect(self.ui.setInfos)
+
+    def updateUi(self, data):
+        structure = data.structure()
+        self.dataStructureChanged.emit(structure, isinstance(data, GridDataDict))
+
+    @QtCore.pyqtSlot(tuple)
+    def _setXYAxes(self, xy):
+        self.xyAxes = xy
+
+    @QtCore.pyqtSlot(dict)
+    def _setReductions(self, reductionsEncoded):
+        reductions = DimensionReducer.decodeReductions(reductionsEncoded)
+        self.reductions = reductions
 
