3b068b5e56a135662b50f589ae2f7fa25277efef
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2018-12-30 15:11:25 +0100

    * cleaning up old code * added some docs * moved checks to validation method

--- plottr/node/data_selector.py
+++ plottr/node/data_selector.py
@@ -5,6 +5,7 @@ A node and widget for subselecting from a dataset.
 """
 from pprint import pprint
 import copy
+from typing import Union, List, Tuple
 
 import numpy as np
 
@@ -93,7 +94,6 @@ class DataTable(QtGui.QTreeWidget):
     def signalSelection(self):
         if self._emitSelection:
             selected = self.getSelected()
-            # print('emit', selected)
             self.selectionChanged.emit(selected)
 
 
@@ -111,13 +111,6 @@ class DataDisplayWidget(QtGui.QWidget):
         self.tbl = DataTable(self, readonly=readonly)
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.tbl)
-
-        # if not readonly:
-        #     self.gridchk = QtGui.QCheckBox()
-        #     self.gridlbl = QtGui.QLabel('Place data on grid')
-        #     gridlayout = QtGui.QFormLayout()
-        #     gridlayout.addRow(self.gridlbl, self.gridchk)
-        #     layout.addLayout(gridlayout)
 
         layout.addStretch()
         self.setLayout(layout)
@@ -150,15 +143,22 @@ class DataDisplayWidget(QtGui.QWidget):
 
 
 class DataSelector(Node):
+    """
+    This node allows extracting data from datasets. The fields specified by
+    `selectedData' and their axes are kept, the rest is discarded in the output.
+    All selected data fields must be compatible in the sense that they have the
+    same axes (also in the same order).
+    The utility of this node is that afterwards data can safely be processed
+    together, as the structure of all remaining fields is shared.
+
+    Properties of this node:
+        * selectedData : a list/tuple of data field names
+    """
 
     nodeName = "DataSelector"
     uiClass = DataDisplayWidget
 
     guiOptions = {
-        # 'grid' : {
-        #     'widget' : 'gridchk',
-        #     'setFunc' : 'setChecked',
-        # },
         'selectedData' : {
             'widget' : None,
             'setFunc' : 'setSelected',
@@ -181,7 +181,7 @@ class DataSelector(Node):
 
     @selectedData.setter
     @Node.updateOption('selectedData')
-    def selectedData(self, val):
+    def selectedData(self, val: List[str]):
         """
         Set the selected data fields to val, which is a list of names of the fields.
         Checks if the datafields are compatible in axes and will raise ValueError
@@ -190,73 +190,53 @@ class DataSelector(Node):
         if isinstance(val, str):
             val = [val]
 
-        vals = []
-        if len(val) > 0:
-            allowed_axes = self._dataStructure[val[0]]['axes']
-            for v in val:
-                axes = self._dataStructure[v]['axes']
-                if axes == allowed_axes:
-                    vals.append(v)
-                else:
-                    raise ValueError(f'Datasets {vals[0]}(with axes {allowed_axes}) '
-                                     f'and {v}(with axes {axes}) are not compatible '
-                                     f'and cannot be selected simultanously.')
-
-        self._selectedData = vals
+        self._selectedData = val
 
     ### Data processing
+
+    def validateOptions(self, data):
+        if data is None:
+            return True
+
+        if len(self.selectedData) > 0:
+            allowed_axes = data.axes(self.selectedData[0])
+            for d in self.selectedData:
+                if data.axes(d) != allowed_axes:
+                    self.logger().error(
+                        f'Datasets {self.selectedData[0]} (with axes {allowed_axes}) '
+                        f'and {d}(with axes {data.axes(d)}) are not compatible '
+                        f'and cannot be selected simultanously.'
+                        )
+                    return False
+        return True
+
 
     def _reduceData(self, data):
         if isinstance(self.selectedData, str):
             dnames = [self.selectedData]
         else:
             dnames = self.selectedData
-
-        # if self.grid or isinstance(data, GridDataDict):
-        #     _data = togrid(data, names=dnames)
-        # else:
-        # _data = DataDict()
-        # for n in dnames:
-        #     _data[n] = data[n]
-        #     for k, v in data.items():
-        #         if k in data[n].get('axes', []):
-        #             _data[k] = v
-        # return _data
-
         return data.extract(dnames)
 
     def process(self, **kw):
-        data = kw['dataIn']
-
+        data = super().process(**kw)
         if data is None:
-            struct = None
-        else:
-            struct = data.structure()
+            return None
+        data = data['dataOut']
+        struct = data.structure()
 
         if not DataDict.same_structure(struct, self._dataStructure):
             self._dataStructure = struct
             self.newDataStructure.emit(struct)
 
-        # if isinstance(data, GridDataDict) and self.ui is not None:
-        #     self.ui.gridchk.setDisabled(True)
-        # elif not isinstance(data, GridDataDict) and self.ui is not None:
-        #     self.ui.gridchk.setDisabled(False)
-
         data = self._reduceData(data)
         return dict(dataOut=data)
-
 
     ### Methods for GUI interaction
 
     def setupUi(self):
         self.newDataStructure.connect(self.ui.setDataStructure)
-        # self.ui.gridchk.setChecked(self._grid)
-        # self.ui.gridchk.stateChanged.connect(self._setGrid)
         self.ui.dataSelected.connect(self._setSelected)
-
-    # @QtCore.pyqtSlot(int)
-    # def _setGrid(self, val):
-    #     self.grid = bool(val)
 
     @QtCore.pyqtSlot(list)
     def _setSelected(self, vals):
