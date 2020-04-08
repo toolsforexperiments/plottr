cc794206f2a1df8a391de6feb155ae96be1acb39
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2020-04-08 09:30:17 +0200

    extended the testing a little.

--- test/gui/simple_2d_plot.py
+++ test/gui/simple_2d_plot.py
@@ -6,7 +6,7 @@ from plottr import log
 from plottr.data.datadict import DataDict, datadict_to_meshgrid
 from plottr.utils import testdata
 from plottr.gui.widgets import SinglePlotWindow
-from plottr.plot.mpl import MPLPlotWidget
+from plottr.plot.mpl import AutoPlot
 
 
 def setup_logging():
@@ -22,22 +22,54 @@ logger = setup_logging()
 def simple_2d_plot():
     app = QtGui.QApplication([])
     win = SinglePlotWindow()
-    plot = MPLPlotWidget(parent=win)
+    plot = AutoPlot(parent=win)
     win.plot.setPlotWidget(plot)
     win.show()
 
-    t0 = time.perf_counter()
-    nsamples = 30
-    for i in range(nsamples):
-        data = datadict_to_meshgrid(testdata.one_2d_set(201, 151))
-        win.plot.setData(data)
+    # plotting 1d traces
+    if False:
+        logger.info(f"1D trace")
+        t0 = time.perf_counter()
+        nsamples = 30
+        for i in range(nsamples):
+            data = datadict_to_meshgrid(
+                testdata.get_1d_scalar_cos_data(201, 2)
+            )
+            win.plot.setData(data)
+        t1 = time.perf_counter()
+        fps = nsamples/(t1-t0)
+        logger.info(f"Performance: {fps} FPS")
 
-    t1 = time.perf_counter()
-    fps = nsamples/(t1-t0)
-    logger.info(f"Performance: {fps} FPS")
+    # plotting images
+    if True:
+        logger.info(f"2D image")
+        t0 = time.perf_counter()
+        nsamples = 30
+        for i in range(nsamples):
+            data = datadict_to_meshgrid(
+                testdata.get_2d_scalar_cos_data(201, 151, 1)
+            )
+            win.plot.setData(data)
+        t1 = time.perf_counter()
+        fps = nsamples/(t1-t0)
+        logger.info(f"Performance: {fps} FPS")
+
+    # plotting 2d scatter
+    if False:
+        logger.info(f"2D scatter")
+        t0 = time.perf_counter()
+        nsamples = 30
+        for i in range(nsamples):
+            data = testdata.get_2d_scalar_cos_data(21, 21, 1)
+            win.plot.setData(data)
+        t1 = time.perf_counter()
+        fps = nsamples/(t1-t0)
+        logger.info(f"Performance: {fps} FPS")
 
     return app.exec_()
 
 
 if __name__ == '__main__':
+    from plottr import plottrPath
+    print(plottrPath)
     simple_2d_plot()
