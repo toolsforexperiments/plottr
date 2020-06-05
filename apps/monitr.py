2846ffb39cc0b6061947f802d9d855fb661de5c8
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2020-06-05 10:38:29 +0200

    small improvements. - format/pep8 - changed default refresh interval to 2 seconds

--- apps/monitr.py
+++ apps/monitr.py
@@ -3,9 +3,9 @@ import os
 import argparse
 
 from plottr import QtGui
-QtWidgets = QtGui
-
 from plottr.apps.monitr import Monitr
+
+QtWidgets = QtGui
 
 
 def main(path, refresh_interval):
@@ -18,13 +18,12 @@ def main(path, refresh_interval):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Monitr main application')
     parser.add_argument("path", help="path to monitor for data", default=None)
-    parser.add_argument("-r", "--refresh_interval", default=1,
+    parser.add_argument("-r", "--refresh_interval", default=2,
                         help="interval at which to look for changes in the "
                              "monitored path (in seconds)")
     args = parser.parse_args()
 
     path = os.path.abspath(args.path)
-
     if not (os.path.exists(path) and os.path.isdir(path)):
         print('Invalid path.')
         sys.exit()
