66ac2261e2703704b267c8ee3f7a77fbc761ec63
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-08-05 12:03:46 +0200

    move inspectr app into plottr

--- apps/inspectr.py
+++ apps/inspectr.py
@@ -1,28 +1,4 @@
-import sys
-import argparse
-
-from pyqtgraph.Qt import QtCore, QtGui
-
-from plottr import log as plottrlog
 from plottr.apps import inspectr
 
-
-def main(dbPath):
-    app = QtGui.QApplication([])
-    plottrlog.enableStreamHandler(True)
-
-    win = inspectr.inspectr(dbPath=dbPath)
-    win.show()
-
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='inspectr -- sifting through qcodes data.')
-    parser.add_argument('--dbpath', help='path to qcodes .db file',
-                        default=None)
-
-    args = parser.parse_args()
-
-    main(args.dbpath)
+    inspectr.script()
