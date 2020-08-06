0b336acb539625804e673804768051983a29dbf0
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-08-06 08:34:41 +0200

    add long description from readme

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='plottr',
     version='0.1.0',
     description='A tool for live plotting and processing data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Wolfgang Pfaff',
     author_email='wolfgangpfff@gmail.com',
     url='https://github.com/toolsforexperiments/plottr',
