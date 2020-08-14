12e42a23b4d76572e31f5081e677ae888f265a27
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-08-14 14:02:19 +0200

    use qtpy as a polyfill for qt versions

--- plottr/__init__.py
+++ plottr/__init__.py
@@ -1,17 +1,9 @@
 import os
 
-from pyqtgraph.Qt import QtCore, QtGui, QtWidgets, QT_LIB
+from qtpy import QtCore, QtGui, QtWidgets
 
-if QT_LIB == 'PyQt5':
-    Signal = QtCore.pyqtSignal
-    Slot = QtCore.pyqtSlot
-elif QT_LIB == 'PySide2':
-    Signal = QtCore.Signal
-    Slot = QtCore.Slot
-else:
-    raise RuntimeError("Unsupported Qt backend. "
-                       "Plottr supports PyQt5 and PySide2. Got: "
-                       f"{QT_LIB} which is not supported.")
+Signal = QtCore.Signal
+Slot = QtCore.Slot
 
 from pyqtgraph.flowchart import Flowchart as pgFlowchart, Node as pgNode
 Flowchart = pgFlowchart
