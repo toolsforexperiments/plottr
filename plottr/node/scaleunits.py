2645349913239ec87662538c380a915bb9ffdbe4
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2020-11-26 11:29:58 +0100

    add basic widget layout

--- plottr/node/scaleunits.py
+++ plottr/node/scaleunits.py
@@ -1,6 +1,7 @@
 from enum import Enum, unique
 from typing import Optional, Dict
 
+from plottr import QtWidgets
 from plottr.node import Node, NodeWidget, updateOption
 from plottr.data.datadict import DataDictBase
 
@@ -18,10 +19,35 @@ class StateOption(Enum):
     always = 2
 
 
+class ScaleUnitOptionWidget(QtWidgets.QWidget):
+
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
+        super().__init__(parent)
+
+        self.buttons = {
+            StateOption.never: QtWidgets.QRadioButton('Never'),
+            StateOption.simpleunits: QtWidgets.QRadioButton('Simple Units'),
+            StateOption.always: QtWidgets.QRadioButton('Always'),
+        }
+        btnLayout = QtWidgets.QVBoxLayout()
+        self.btnGroup = QtWidgets.QButtonGroup(self)
+
+        for opt in StateOption:
+            btn = self.buttons[opt]
+            self.btnGroup.addButton(btn, opt.value)
+            btnLayout.addWidget(btn)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addLayout(btnLayout)
+        layout.addStretch()
+        self.setLayout(layout)
+        self.buttons[StateOption.never].setChecked(True)
+
+
 class ScaleUnitsWidget(NodeWidget):
 
     def __init__(self, node: Optional[Node] = None):
-        super().__init__(node=node, embedWidgetClass=None)
+        super().__init__(node=node, embedWidgetClass=ScaleUnitOptionWidget)
 
 
 class ScaleUnits(Node):
