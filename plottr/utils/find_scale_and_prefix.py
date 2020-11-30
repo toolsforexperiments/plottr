3d945eaf23d8693d60a32a1107d4671955d633e5
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2020-11-30 08:43:22 +0100

    sync fixes from qcodes

--- plottr/utils/find_scale_and_prefix.py
+++ plottr/utils/find_scale_and_prefix.py
@@ -48,15 +48,16 @@ def find_scale_and_prefix(data: np.ndarray, unit: str) -> Tuple[str, int]:
     and matching scale that best describes the data.
 
     The units for which unit prefixes are added can be found in
-    `_UNITS_FOR_RESCALING`. For all other units
-    the prefix is an exponential scaling factor e.g. `10^3 `.
+    ``_UNITS_FOR_RESCALING``. For all other units
+    the prefix is an exponential scaling factor e.g. ``10^3``.
 
     Args:
         data: A numpy array of data.
         unit: The unit the data is measured in.
             Should not contain a prefix already.
 
-    Returns: tuple of prefix and exponential scale
+    Returns:
+        tuple of prefix and exponential scale
 
     """
     maxval = np.nanmax(np.abs(data))
@@ -67,7 +68,7 @@ def find_scale_and_prefix(data: np.ndarray, unit: str) -> Tuple[str, int]:
                 selected_scale = 0
                 prefix = _ENGINEERING_PREFIXES[selected_scale]
                 break
-            elif maxval < threshold:
+            if maxval < threshold:
                 selected_scale = scale
                 prefix = _ENGINEERING_PREFIXES[scale]
                 break
