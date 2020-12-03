fa6c6f804bacc225efc6b8819c8fd7f585559996
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2020-12-02 20:03:09 -0600

    moved things up a level that are basically plotting backend-independent. Added new class that is supposed to handle arranging plots into different panels (no need to mix that with actual plotting code).

--- plottr/plot/base.py
+++ plottr/plot/base.py
@@ -1,12 +1,22 @@
 """
 plottr/plot/base.py : Contains the base classes for plotting nodes and widgets.
+
+Most things that are independent of plotting backend are in here.
 """
 
-from typing import Dict, List, Type, Tuple, Optional
+from enum import Enum, unique, auto
+from typing import Dict, List, Type, Tuple, Optional, Any, \
+    OrderedDict as OrderedDictType
+from collections import OrderedDict
+from dataclasses import dataclass
+from copy import copy, deepcopy
+
+import numpy as np
 
 from .. import Signal, Flowchart, QtWidgets
-from ..data.datadict import DataDictBase
+from ..data.datadict import DataDictBase, DataDict, MeshgridDataDict
 from ..node import Node, linearFlowchart
+
 
 __author__ = 'Wolfgang Pfaff'
 __license__ = 'MIT'
@@ -134,3 +144,315 @@ def makeFlowchartWithPlot(nodes: List[Tuple[str, Type[Node]]],
     nodes.append((plotNodeName, PlotNode))
     fc = linearFlowchart(*nodes)
     return fc
+
+
+# Types of plots and plottable data
+@unique
+class PlotDataType(Enum):
+    """Types of (plotable) data"""
+
+    #: unplottable data
+    unknown = auto()
+
+    #: scatter-type data with 1 dependent (data is not on a grid)
+    scatter1d = auto()
+
+    #: line data with 1 dependent (data is on a grid)
+    line1d = auto()
+
+    #: scatter data with 2 dependents (data is not on a grid)
+    scatter2d = auto()
+
+    #: grid data with 2 dependents
+    grid2d = auto()
+
+
+@unique
+class PlotType(Enum):
+    """Plot types"""
+
+    #: no plot defined
+    empty = auto()
+
+    #: a single 1D line/scatter plot per panel
+    singletraces = auto()
+
+    #: multiple 1D lines/scatter plots per panel
+    multitraces = auto()
+
+    #: image plot of 2D data
+    image = auto()
+
+    #: colormesh plot of 2D data
+    colormesh = auto()
+
+    #: 2D scatter plot
+    scatter2d = auto()
+
+
+@unique
+class ComplexRepresentation(Enum):
+    """Options for plotting complex-valued data."""
+
+    #: only real
+    real = auto()
+
+    #: real and imaginary
+    realAndImag = auto()
+
+    #: magnitude and phase
+    magAndPhase = auto()
+
+
+def determinePlotDataType(data: Optional[DataDictBase]) -> PlotDataType:
+    """
+    Analyze input data and determine most likely :class:`PlotDataType`.
+
+    Analysis is simply based on number of dependents and data type.
+
+    :param data: data to analyze.
+    """
+    # TODO:
+    #   there's probably ways to be more liberal about what can be plotted.
+    #   like i can always make a 1d scatter...
+
+    # a few things will result in unplottable data:
+    # * wrong data format
+    if not isinstance(data, DataDictBase):
+        return PlotDataType.unknown
+
+    # * incompatible independents
+    if not data.axes_are_compatible():
+        return PlotDataType.unknown
+
+    # * too few or too many independents
+    if len(data.axes()) < 1 or len(data.axes()) > 2:
+        return PlotDataType.unknown
+
+    # * no data to plot
+    if len(data.dependents()) == 0:
+        return PlotDataType.unknown
+
+    if isinstance(data, MeshgridDataDict):
+        shape = data.shapes()[data.dependents()[0]]
+
+        if len(data.axes()) == 2:
+            return PlotDataType.grid2d
+        else:
+            return PlotDataType.line1d
+
+    elif isinstance(data, DataDict):
+        if len(data.axes()) == 2:
+            return PlotDataType.scatter2d
+        else:
+            return PlotDataType.scatter1d
+
+    return PlotDataType.unknown
+
+
+@dataclass
+class PlotItem:
+    data: List[np.ndarray]
+    id: int
+    subPlot: int
+    labels: Optional[List[str]] = None
+    style: Optional[str] = None
+    plotOptions: Optional[Dict[str, Any]] = None
+    plotReturn: Optional[Any] = None
+
+
+@dataclass
+class SubPlot:
+    id: int
+    axes: Optional[List[Any]] = None
+
+
+class AutoFigureMaker(object):
+    """A class for semi-automatic creation of plot figures.
+    It must be inherited to tie it to a specific plotting backend.
+
+    The main purpose of this class is to (a) implement actual plotting of
+    plot items, and (b) distribute plot items correctly among subpanels of
+    a figure.
+
+    FigureMaker is a context manager. The user should eventually only need to
+    add data and specify what kind of data it is. FigureMaker will then
+    generate plots from that.
+
+    In the simplest form, usage looks something like this::
+
+        >>> with FigureMaker() as fm:
+        >>>     fm.addData(x, y, [...])
+        >>>     [...]
+
+    See :method:`addData` for details on how to specify data and how to pass
+    plot options to it.
+    """
+
+    # TODO: implement feature for always plotting certain traces with other
+    #   other ones ('children'). This is mainly used for models/fits.
+    #   needs a system to copy certain style aspects from the parents.
+    # TODO: similar, but with siblings (imagine Re/Im parts)
+
+    def __init__(self):
+        self.subPlots: OrderedDictType[int, SubPlot] = OrderedDict()
+        self.plotItems: OrderedDictType[int, PlotItem] = OrderedDict()
+
+        #: how to represent complex data.
+        #: must be set before adding data to the plot to have an effect.
+        self.complex_representation = ComplexRepresentation.realAndImag
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._makeAxes()
+        for id in self.subPlots.keys():
+            self._makeSubPlot(id)
+
+    # private methods
+    def _makeAxes(self) -> None:
+        n = self.nSubPlots()
+        for id, axes in zip(range(n), self.makeAxes(n)):
+            if not isinstance(axes, list):
+                axes = [axes]
+            self.subPlots[id] = SubPlot(id, axes)
+
+    def addPanel(self) -> int:
+        id = _generate_auto_dict_key(self.subPlots)
+        self.subPlots[id] = SubPlot(id, [])
+        return id
+
+    def _makeSubPlot(self, id: int):
+        items = self.subPlotItems(id)
+        for name, item in items.items():
+            self._plot(item)
+        self.formatSubPlot(id)
+
+    def _plot(self, plotItem: PlotItem):
+        if plotItem.style is None:
+            if len(plotItem.data) == 2:
+                style = 'line'
+            elif len(plotItem.data) == 3:
+                style = 'image'
+            else:
+                raise ValueError("Cannot automatically determine plot style.")
+        else:
+            style = plotItem.style
+
+        if hasattr(self, f'plot_{style}'):
+            plotFunc = getattr(self, f'plot_{style}')
+        else:
+            raise ValueError(f"No plot function for style '{style}'.")
+        plotItem.plotReturn = plotFunc(plotItem)
+
+    def _splitComplexData(self, plotItem: PlotItem) -> List[PlotItem]:
+        if not np.issubsctype(plotItem.data[-1], np.complexfloating):
+            return [plotItem]
+
+        label = plotItem.labels[-1]
+        if self.complex_representation is ComplexRepresentation.realAndImag:
+            re_data = plotItem.data[-1].real
+            im_data = plotItem.data[-1].imag
+
+            if label == '':
+                re_label, im_label = 'Real', 'Imag'
+            else:
+                re_label, im_label = label + ' (Real)', label + ' (Imag)'
+
+            re_plotItem = plotItem
+            im_plotItem = deepcopy(re_plotItem)
+
+            # FIXME: for > 1d data we should have separate panels!
+            re_plotItem.data[-1] = re_data
+            re_plotItem.labels[-1] = re_label
+            im_plotItem.data[-1] = im_data
+            im_plotItem.labels[-1] = im_label
+            im_plotItem.id = re_plotItem.id + 1
+            return [re_plotItem, im_plotItem]
+
+        if self.complex_representation is ComplexRepresentation.magAndPhase:
+            mag_data = np.abs(plotItem.data[-1])
+            phase_data = np.angle(plotItem.data[-1])
+
+            if label == '':
+                mag_label, phase_label = 'Mag', 'Phase'
+            else:
+                mag_label, phase_label = label + ' (Mag)', label + ' (Phase)'
+
+            mag_plotItem = plotItem
+            phase_plotItem = deepcopy(mag_plotItem)
+
+            mag_plotItem.data[-1] = mag_data
+            mag_plotItem.labels[-1] = mag_label
+            phase_plotItem.data[-1] = phase_data
+            phase_plotItem.labels[-1] = phase_label
+            phase_plotItem.id = mag_plotItem.id + 1
+            phase_plotItem.subPlot = mag_plotItem.subPlot + 1
+            return [mag_plotItem, phase_plotItem]
+
+    # public methods
+    def nSubPlots(self):
+        ids = []
+        for id, item in self.plotItems.items():
+            ids.append(item.subPlot)
+        return len(set(ids))
+
+    def subPlotItems(self, subPlotId: int) -> OrderedDictType[int, PlotItem]:
+        items = OrderedDict()
+        for id, item in self.plotItems.items():
+            if item.subPlot == subPlotId:
+                items[id] = item
+        return items
+
+    def subPlotLabels(self, subPlotId: int) -> List[List[str]]:
+        ret = []
+        items = self.subPlotItems(subPlotId)
+        for id, item in items.items():
+            if item.labels is not None:
+                for i, l in enumerate(item.labels):
+                    while(len(ret)) <= i:
+                        ret.append([])
+                    ret[i].append(l)
+        return ret
+
+    def addData(self, *data: np.ndarray, join: Optional[int] = None,
+                labels: Optional[List[str]] = None, style: Optional[str] = None,
+                **plotOptions: Any) -> int:
+
+        id = _generate_auto_dict_key(self.plotItems)
+        if join is None:
+            subPlotId = self.nSubPlots()
+        else:
+            subPlotId = self.plotItems[join].subPlot
+
+        if labels is None:
+            labels = [''] * len(data)
+
+        plotItem = PlotItem(list(data), id, subPlotId,
+                            labels, style, plotOptions)
+
+        for p in self._splitComplexData(plotItem):
+            self.plotItems[p.id] = p
+
+        return id
+
+    # Methods to be implemented by inheriting classes
+    def makeAxes(self, nSubPlots: int) -> List[Any]:
+        raise NotImplementedError
+
+    def formatSubPlot(self, subPlotId: int):
+        pass
+
+    def plot_line(self, plotItem: PlotItem):
+        raise NotImplementedError
+
+    def plot_image(self, plotItem: PlotItem):
+        raise NotImplementedError
+
+
+def _generate_auto_dict_key(d: Dict):
+    guess = 0
+    while guess in d.keys():
+        guess += 1
+    return guess
