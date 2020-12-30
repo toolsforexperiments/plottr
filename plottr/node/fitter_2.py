fb8cb821e918de315811c50d3e16b9abd0bdd021
Chao Zhou <chz78@pitt.edu>
2020-12-30 18:38:22 -0500

    working on fitting node

--- plottr/node/fitter_2.py
+++ plottr/node/fitter_2.py
@@ -68,8 +68,8 @@ INITIAL_MODULES = get_modules_in_pkg(fitters)
 #TODO: this requires putting the modules in the init of fitters, there should
 # be a better way to do this.
 
-OPEN_MODULE_ICON = QtGui.QIcon(QtWidgets.QApplication.style().standardIcon(QtWidgets.QStyle.SP_DirOpenIcon))
-REFRESH_MODULE_ICON = QtGui.QIcon(QtWidgets.QApplication.style().standardIcon(QtWidgets.QStyle.SP_BrowserReload))
+# OPEN_MODULE_ICON = QtGui.QIcon(QtWidgets.QApplication.style().standardIcon(QtWidgets.QStyle.SP_DirOpenIcon))
+# REFRESH_MODULE_ICON = QtGui.QIcon(QtWidgets.QApplication.style().standardIcon(QtWidgets.QStyle.SP_BrowserReload))
 
 
 MAX_FLOAT = sys.float_info.max
@@ -111,13 +111,15 @@ class FittingGui(NodeWidget):
                                         QtWidgets.QSizePolicy.Expanding)
         module_sel_grid.addWidget(self.module_combo, 0, 0)
         # refresh module button
-        refresh_button = QtWidgets.QPushButton(REFRESH_MODULE_ICON, "")
+        # refresh_button = QtWidgets.QPushButton(REFRESH_MODULE_ICON, "")
+        refresh_button = QtWidgets.QPushButton("↻")
         refresh_button.clicked.connect(self.moduleRefreshClicked)
         refresh_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                      QtWidgets.QSizePolicy.Fixed)
         module_sel_grid.addWidget(refresh_button, 0, 1)
         # add module button
-        open_button = QtWidgets.QPushButton(OPEN_MODULE_ICON,"")
+        # open_button = QtWidgets.QPushButton(OPEN_MODULE_ICON,"")
+        open_button = QtWidgets.QPushButton("+")
         open_button.clicked.connect(self.add_user_module)
         open_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                   QtWidgets.QSizePolicy.Fixed)
@@ -138,6 +140,20 @@ class FittingGui(NodeWidget):
         self.layout.addWidget(self.model_doc_box)
 
 
+        # parameter table
+        self.param_table = QtWidgets.QTableWidget(0, 4)
+        self.param_table.setHorizontalHeaderLabels([
+            'fix', 'initial guess', 'lower bound', 'upper bound'])
+        self.param_table.horizontalHeader(). \
+            setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        self.layout.addWidget(self.param_table)
+
+        # fitting update options
+        self.addUpdateOptions()
+
+        # getter and setter
+        # self.optGetters['fitting_model'] = self.fittingOptionGetter
+        # self.optSetters['fitting_model'] = self.fittingOptionSetter
     def addModuleComboBox(self):
         """ Set up the model function drop down manual widget.
         """
@@ -218,12 +234,194 @@ class FittingGui(NodeWidget):
         if current is None:
             print ("No model selected")
             self.model_doc_box.setText("")
+            self.param_table.setRowCount(0)
             return
         current_module = self.fitting_modules[self.module_combo.currentText()]
         model_cls = getattr(current_module, current.text())
-        print(model_cls.model.__doc__)
-
+        print(model_cls.model.__code__)
+        self.updateParamTable(model_cls)
         self.model_doc_box.setText(model_cls.model.__doc__)
+
+    def updateParamTable(self, model_cls: Type[Fit]):
+        """ Update the parameter table based on the current model selection.
+        :param model_cls: the current selected fitting model class
+        """
+        # flush param table
+        self.param_table.setRowCount(0)
+        # rebuild param table based on the selected model function
+        func = model_cls.model
+        # assume the first variable is the independent variable
+        params = list(inspect.signature(func).parameters)[1:]
+        self.param_table.setRowCount(len(params))
+        self.param_table.setVerticalHeaderLabels(params)
+        # generate fix, initial guess, lower/upper bound option GUIs for each
+        # parameter
+        self.param_signals = []
+        for idx, name in enumerate(params):
+            fixParamCheck = self._paramFixCheck()
+            fixParamCheck.setStyleSheet("margin-left:10%; margin-right:10%;")
+            initialGuessBox = OptionSpinbox(1.0, self)
+            lowerBoundBox = NumberInput(None, self)
+            upperBoundBox = NumberInput(None, self)
+            lowerBoundBox.newTextEntered.connect(initialGuessBox.setMinimum)
+            upperBoundBox.newTextEntered.connect(initialGuessBox.setMaximum)
+
+            # gather the param change signals for enabling live update
+            self.param_signals.extend((fixParamCheck.stateChanged,
+                                       initialGuessBox.valueChanged,
+                                       lowerBoundBox.newTextEntered,
+                                       upperBoundBox.newTextEntered))
+            # put param options into table
+            self.param_table.setCellWidget(idx, 0, fixParamCheck)
+            self.param_table.setCellWidget(idx, 1, initialGuessBox)
+            self.param_table.setCellWidget(idx, 2, lowerBoundBox)
+            self.param_table.setCellWidget(idx, 3, upperBoundBox)
+
+        self.changeParamLiveUpdate(self.live_update)
+
+    def _paramFixCheck(self, default_value: bool = False):
+        """generate a push checkbox for the parameter fix option.
+        :param default_value : param is fixed by default or not
+        :returns: a checkbox widget
+        """
+        widget = QtWidgets.QCheckBox('')
+        widget.setChecked(default_value)
+        widget.setToolTip("when fixed, the parameter will be fixed to the "
+                          "initial guess value during fitting")
+        return widget
+
+    def addUpdateOptions(self):
+        ''' Add check box & buttons that control the fitting update policy.
+        '''
+        widget = QtWidgets.QWidget()
+        grid = QtWidgets.QGridLayout()
+        # when checked, fitting will update after each change of fitting model
+        # or parameter option
+        liveUpdateCheck = QtWidgets.QCheckBox('Live Update')
+        grid.addWidget(liveUpdateCheck, 0, 0)
+        # update fitting on-demand
+        updateButton = QtWidgets.QPushButton("Update")
+        grid.addWidget(updateButton, 0, 1)
+        # reload the fitting options that come from the data
+        reloadInputOptButton = QtWidgets.QPushButton("Reload Input Option")
+        grid.addWidget(reloadInputOptButton, 0, 2)
+
+        @Slot(QtCore.Qt.CheckState)
+        def setLiveUpdate(live: QtCore.Qt.CheckState):
+            ''' connect/disconnects the changing signal of each fitting
+            option to signalAllOptions slot
+            '''
+            if live == QtCore.Qt.Checked:
+                self.model_list.currentItemChanged.connect(self._signalAllOptions)
+                self.changeParamLiveUpdate(True)
+                self.live_update = True
+            else:
+                try:
+                    self.model_list.currentItemChanged.disconnect(
+                        self._signalAllOptions)
+                except TypeError:
+                    pass
+                self.changeParamLiveUpdate(False)
+                self.live_update = False
+
+        @Slot()
+        def reloadInputOption():
+            print("reload input option")
+            # self.fittingOptionSetter(self.input_options)
+
+        liveUpdateCheck.stateChanged.connect(setLiveUpdate)
+        updateButton.pressed.connect(self.signalAllOptions)
+        reloadInputOptButton.pressed.connect(reloadInputOption)
+        reloadInputOptButton.setToolTip('reload the fitting options stored '
+                                        'in the input data')
+
+        widget.setLayout(grid)
+        self.layout.addWidget(widget)
+
+    def changeParamLiveUpdate(self, enable: bool):
+        ''' connect/disconnects the changing signal of each fitting param
+        option to signalAllOptions slot
+        :param enable: connect/disconnect when enable is True/False.
+        '''
+        if enable:
+            for psig in self.param_signals:
+                psig.connect(self._signalAllOptions)
+        else:
+            for psig in self.param_signals:
+                try:
+                    psig.disconnect(self._signalAllOptions)
+                except TypeError:
+                    pass
+
+
+
+    def _signalAllOptions(self, *args):
+        # to make the signalAllOptions accept signals w/ multi args
+        print("signal option change")
+        self.signalAllOptions()
+
+class OptionSpinbox(QtWidgets.QDoubleSpinBox):
+    """A spinBox widget for parameter options
+    :param default_value : default value of the option
+    """
+
+    # TODO: Support easier input for large numbers
+    def __init__(self, default_value=1.0, parent=None):
+        super().__init__(parent)
+        self.setRange(-1 * MAX_FLOAT, MAX_FLOAT)
+        self.setValue(default_value)
+
+    def setMaximum(self, maximum):
+        try:
+            value = eval(maximum)
+        except:
+            value = MAX_FLOAT
+        if isinstance(value, numbers.Number):
+            super().setMaximum(value)
+        else:
+            super().setMaximum(MAX_FLOAT)
+
+    def setMinimum(self, minimum):
+        try:
+            value = eval(minimum)
+        except:
+            value = -1 * MAX_FLOAT
+        if isinstance(value, numbers.Number):
+            super().setMinimum(value)
+        else:
+            super().setMinimum(-1 * MAX_FLOAT)
+
+
+class NumberInput(QtWidgets.QLineEdit):
+    """A text edit widget that checks whether its input can be read as a
+    number.
+    This is copied form the parameter GUI that Wolfgang wrote for the
+    parameter manager gui.
+    """
+    newTextEntered = Signal(str)
+
+    def __init__(self, default_value, parent=None):
+        super().__init__(parent)
+        self.setValue(default_value)
+        self.editingFinished.connect(self.emitNewText)
+
+    def value(self):
+        try:
+            value = eval(self.text())
+        except:
+            return None
+        if isinstance(value, numbers.Number):
+            return value
+        else:
+            return None
+
+    def setValue(self, value):
+        self.setText(str(value))
+
+    def emitNewText(self):
+        self.newTextEntered.emit(self.text())
+
+
 # ================= Node ==============================
 class FittingNode(Node):
     uiClass = FittingGui
