47c34de68440c46ae33e2fc38ba6b4f46a6786a9
WilliamHPNielsen <whpn@mailbox.org>
2021-02-18 11:13:00 +0100

    Add version log message to main __init__

--- plottr/__init__.py
+++ plottr/__init__.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 import os
+import logging
 
 if TYPE_CHECKING:
     from PyQt5 import QtCore, QtGui, QtWidgets
@@ -19,3 +20,6 @@ plottrPath = os.path.split(os.path.abspath(__file__))[0]
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+log = logging.getLogger(__name__)
+log.info(f"Imported plottr version: {__version__}")
