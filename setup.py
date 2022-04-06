dc94f916ce81047aa97a52130dee33383905cf78
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-04-06 08:41:01 +0200

    replace use of versioneer with versioningit

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
-import os
-import sys
-
 from setuptools import setup
-
-# we need to import versioneer from the current dir
-# once versioneer is pep517/518 compliant this can be removed
-sys.path.append(os.path.dirname(__file__))
-import versioneer
-
-sys.path.pop()
+from versioningit import get_cmdclasses
 
 if __name__ == "__main__":
     setup(
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
+        cmdclass=get_cmdclasses(),
     )
