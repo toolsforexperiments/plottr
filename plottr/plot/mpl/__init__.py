4181f965af17b9af4c5048240614a81b46c1087d
pfafflabatuiuc <72898175+pfafflabatuiuc@users.noreply.github.com>
2022-11-05 15:24:47 -0500

    Update __init__.py

--- plottr/plot/mpl/__init__.py
+++ plottr/plot/mpl/__init__.py
@@ -45,10 +45,10 @@ from matplotlib import rcParams, colormaps, pyplot as plt
 
 from plottr import configFiles
 from .autoplot import AutoPlot, FigureMaker
-from .widgets import MPLPlot, MPLPlotWidget
+# from .widgets import MPLPlot, MPLPlotWidget
 
 
 logger = logging.getLogger(__name__)
 
 # matplotlib tools and settings
-symmetric_cmap = colormaps.get_cmap('bwr')
+# symmetric_cmap = colormaps.get_cmap('bwr')
