b4f705899e5b50a4101b4b6ab9eb6cd665510bf3
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2024-11-07 15:47:56 +0100

    Correct import

--- plottr/config/plottrcfg_main.py
+++ plottr/config/plottrcfg_main.py
@@ -1,4 +1,4 @@
-from cycler import cycler
+from matplotlib.rcsetup import cycler
 from plottr.plot.pyqtgraph.autoplot import AutoPlot as PGAutoPlot
 from plottr.plot.mpl.autoplot import AutoPlot as MPLAutoPlot
 
